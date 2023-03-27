ff814f7ac36b7c7aba6478d253ea64bc282cbfe1
Diogo Schoninger <schoninger.diogo@gmail.com>
2023-03-27 17:08:07 -0300

    Error handlers
    - Implementados handlers de erros para enviar erros inesperados sem derrubar o servidor.

--- crud/src/routes.js
+++ crud/src/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
+import asyncErrorHandler from './middlewares/async-error.js'
 
 const router = Router()
 
 router
-  .post('/', (_req, res) => res.status(204).send())
-  .get('/', (_req, res) => res.status(204).send())
-  .get('/:id', (_req, res) => res.status(204).send())
-  .put('/:id', (_req, res) => res.status(204).send())
-  .delete('/:id', (_req, res) => res.status(204).send())
+  .post('/', asyncErrorHandler((_req, res) => res.status(204).send()))
+  .get('/', asyncErrorHandler((_req, res) => res.status(204).send()))
+  .get('/:id', asyncErrorHandler((_req, res) => res.status(204).send()))
+  .put('/:id', asyncErrorHandler((_req, res) => res.status(204).send()))
+  .delete('/:id', asyncErrorHandler((_req, res) => res.status(204).send()))
 
 export default router
--- crud/src/server.js
+++ crud/src/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+
+import errorHandler from './middlewares/error.js'
 import users from './routes.js'
 
 const app = express()
@@ -6,6 +8,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/users', users)
+
+app.use(errorHandler())
 
 app.use('*', (_req, res) => {
   res.status(404).send({ message: "Invalid endpoint" })
